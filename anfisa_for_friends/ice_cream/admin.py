c29ec453c2ff53d5d6356a9679fe7bf06298afc4
unknown <darck2014@mail.ru>
2023-11-23 10:41:09 +0400

    changed anfisa

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Wrapper, IceCream, Topping
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
+admin.site.register(Topping)
+    
